c89500892d42a6ce1dd223c43f011f4776a82b25
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:47:28 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:47:15.633Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-08T20:47:27.950Z
+Commit: Fix broken links in documentation
