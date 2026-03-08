830b57a48c593209d508c6373721830e3b414fd9
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:48:03 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:47:57.698Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T20:48:03.165Z
+Commit: Fix broken links in documentation
