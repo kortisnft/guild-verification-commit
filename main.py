9ffb33e55c5f1eb0607714b13ac181ec74858f2f
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:48:13 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:48:08.687Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-08T20:48:12.566Z
+Commit: Fix memory leak in event handlers
