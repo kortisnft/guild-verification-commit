63c2656ed2a6bd249f5c13aeabbfaa130afbfed7
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:46:45 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:46:05.226Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T20:46:44.599Z
+Commit: Patch API endpoint error handling
