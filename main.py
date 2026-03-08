2fb12e99ba636c3a1e57262f8c99c6775d2327db
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:46:05 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:45:59.732Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-08T20:46:05.226Z
+Commit: Introduce real-time notifications
