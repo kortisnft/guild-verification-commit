ab6064f5a8d76d8b5ca13ac9441c1035a3a1e47a
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:46:37 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:46:23.699Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-08T20:46:36.549Z
+Commit: Fix CSS styling bug on mobile devices
