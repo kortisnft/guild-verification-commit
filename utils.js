56ee4f73a7cd71c968d5262f40a78662f9b89fdd
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:47:58 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:47:55.016Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T20:47:57.698Z
+Commit: Resolve timezone issues in date handling
