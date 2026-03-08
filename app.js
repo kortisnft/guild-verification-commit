978498d1552f01b19278156a9963395f72f0cae6
kortisnft <160186690+kortisnft@users.noreply.github.com>
2026-03-08 23:47:44 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T20:47:36.416Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T20:47:43.750Z
+Commit: Fix broken links in documentation
